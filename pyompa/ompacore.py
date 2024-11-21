6ff0c8842531d57161fb9be4ab2e4ed57cbf3d88
Giordy13 <131989301+Giordy13@users.noreply.github.com>
2024-11-21 15:36:23 +0100

    SMART_4

--- pyompa/ompacore.py
+++ pyompa/ompacore.py
@@ -1093,7 +1093,7 @@ class OMPAProblem(object):
             liw_index = 1
             wmdw_index = 2
 
-            density_mask = self.potential_density1000 < 33.45
+            density_mask = self.potential_density1000 < 33.42
             
             # Strictly enforce F_WMDW = 0 where density < 33.44
             constraints.append(x[density_mask, wmdw_index] == 0)
