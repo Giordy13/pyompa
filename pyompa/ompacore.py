93e45d8a8ac027cac9225bf69ddd54ae471c4113
Giordy13 <131989301+Giordy13@users.noreply.github.com>
2025-01-03 12:12:20 +0100

    Update ompacore.py

--- pyompa/ompacore.py
+++ pyompa/ompacore.py
@@ -1096,7 +1096,7 @@ class OMPAProblem(object):
             # Define transition margin
             aw_limit = 33.4275    # Above this, no AW
             liw_wmdw_transition = 33.44  # LIW-WMDW transition boundary
-            wmdw_limit = 33.46    # Below this, no WMDW
+            wmdw_limit = 33.45    # Below this, no WMDW
             transition_margin = 0.008  # For smooth transition 
 
             # Create density masks
