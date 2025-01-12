f005fb5539952d9de3a1527e273aa4bf751ac24a
Giordy13 <131989301+Giordy13@users.noreply.github.com>
2025-01-12 13:39:51 +0100

    Update ompacore.py

--- pyompa/ompacore.py
+++ pyompa/ompacore.py
@@ -1087,7 +1087,7 @@ class OMPAProblem(object):
                   x[:,num_endmembers:]) >= 0)
 
     # Add the new density-dependent constraint
-	if self.potential_density1000 is not None:
+        if self.potential_density1000 is not None:
            # Assuming the order of water masses is [AW, LIW, WMDW]
            aw_index = 0
            liw_index = 1
